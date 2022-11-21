108549d57121c9016431f92218ff56b435dac744
VyPham23 <vy.pham200210@vnuk.edu.vn>
2022-11-22 00:30:25 +0700

    update form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,50 +1,63 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import './Form.css';
 
-const Form = () => {
-    const [submitted, setSubmitted] = useState(false);
-    const handleSubmit = () => {
-        setTimeout(() => {
-            setSubmitted(true);
-        }, 100);
-    };
+interface FormInputs {
+    name: string;
+    category: string;
+    description: string;
+    ingredient: string[];
+    price: number;
+  }
 
-    if (submitted) {
-        return (
-            <>
-                
-            </>
-        );
-    }
+const Form = () => {
+    
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+      } = useForm<FormInputs>();
+    
+      const onSubmit = (data: FormInputs) => {
+        console.log(data);
+      };
+
+    
 
     const addInput = () => {
         
     }
+
     return (
         <div className="form__container">
 
-            <form onSubmit={handleSubmit} method="POST">
+            <form onSubmit= {handleSubmit(onSubmit)} method="POST">
                 <h2 id="formtitle">UPDATE DATA</h2>
                 <div className='form__section'>
                     <label>Name(*)</label><br/>
-                    <input type="text" placeholder="Food name" name="name" required />
+                    <input {...register("name")} className='form__input' type="text" placeholder="Food name" required />
+                </div>
+
+                <div className='form__section'>
+                    <label>Description(*)</label><br/>
+                    <input {...register("description")} className='form__input' type="text" placeholder="Description" required />
                 </div>
 
                 <div className='form__section'>
                     <label>Category(*)</label><br/>
-                    <input className='form__input' type="text" placeholder="Category" name="category" required />
+                    <input {...register("category")} className='form__input' type="text" placeholder="Category" required />
                 </div>
 
                 <div className='form__section'>
                     <label>Ingredients:</label><br/>
-                    <input className='form__input' type="text" placeholder="Ingredient" name="ingredient[]"/>
-                    
+                    <input {...register("ingredient")} className='form__input' type="text" placeholder="Ingredient"/>
+                    <input {...register("ingredient")} className='form__input' type="text" placeholder="Ingredient"/>
                     <button onClick={addInput}>+</button>
                 </div>
 
                 <div className='form__section'>
                     <label>Price in VND(*)</label><br/>
-                    <input className='form__input' type="number" min="0" placeholder="Price" name="price" required />
+                    <input {...register("price")} className='form__input' type="number" min="0" placeholder="Price" name="price" required />
                 </div>
                 <div>
                     <button type="submit">Save</button>
